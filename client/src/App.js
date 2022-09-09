b2539599ed36bbd47f25e6d1b35da215d7a9f8e2
Nikita <djneel.nz@gmail.com>
2022-09-09 11:44:57 +0300

    model added in project

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
+// import { Route, Routes } from 'react-router-dom';
+import Scene from './components/Scene/Scene';
 import './App.css';
 
 function App() {
-  useEffect(() => {
-    fetch('http://localhost:3006/test')
-      .then((res) => res.json())
-      .then((res) => console.log(res));
-  });
+  // useEffect(() => {
+  //   fetch('http://localhost:3006/test')
+  //     .then((res) => res.json())
+  //     .then((res) => console.log(res));
+  // });
   return (
     <div className="App">
-      App
+      {/* <Routes>
+        <Route path="/" element={<Scene />} />
+      </Routes> */}
+      <Scene />
     </div>
   );
 }
