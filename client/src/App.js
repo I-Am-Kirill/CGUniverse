3cbd005db819b17674c131a7db07cdf4e5fa52c7
Fedorov Andrey <walkeder@gmail.com>
2022-09-09 14:45:32 +0300

    modalWindow

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
-import { useEffect } from 'react';
+import { Children, useState } from 'react';
 import './App.css';
-import Page from './components/Page/Page';
+import BasketShop from './components/BasketShop/BasketShop';
+import Modal from './components/Modal/Modal';
 
 
 function App() {
-  useEffect(() => {
-    fetch('http://localhost:3006/test')
-      .then((res) => res.json())
-      .then((res) => console.log(res));
-  });
+  const [modalActive, setModalActive] = useState(false);
+  // useEffect(() => {
+  //   fetch('http://localhost:3006/test')
+  //     .then((res) => res.json())
+  //     .then((res) => console.log(res));
+  // });
   return (
     <div className="App">
-      <Page />
+      <button className="open-btn" onClick={() => setModalActive(true)} type="button">open</button>
+      <Modal
+        active={modalActive}
+        setActive={setModalActive}
+      >
+        <BasketShop />
+      </Modal>
     </div>
   );
 }
---
+++ client/src/components/BasketShop/BasketShop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function BasketShop() {
+  return (
+    <div className="basket-container">
+      <div className="basket-content">
+        <h1>hello</h1>
+      </div>
+    </div>
+  );
+}
