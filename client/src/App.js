29a2a7fc02721275f27e0d0b61090b1379038181
Fedorov Andrey <walkeder@gmail.com>
2022-09-09 17:11:52 +0300

    basket

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import { Children, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import BasketShop from './components/BasketShop/BasketShop';
 import Modal from './components/Modal/Modal';
-import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Page from './components/Page/Page';
@@ -11,15 +11,9 @@ import Page from './components/Page/Page';
 
 function App() {
   const [modalActive, setModalActive] = useState(false);
-  // useEffect(() => {
-  //   fetch('http://localhost:3006/test')
-  //     .then((res) => res.json())
-  //     .then((res) => console.log(res));
-  // });
   return (
     <div className="App">
 
-      <button className="open-btn" onClick={() => setModalActive(true)} type="button">open</button>
       <Modal
         active={modalActive}
         setActive={setModalActive}
@@ -27,7 +21,7 @@ function App() {
         <BasketShop />
       </Modal>
 
-      <Header />
+      <Header setModalActive={setModalActive} />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/page" element={<Page />} />
